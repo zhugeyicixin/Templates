655d99abb04b079edc075116f8e84934671f53cf
zhugeyicixin <hetanjin@163.com>
2016-03-15 22:29:13 +0800

    Tem.21.ver.2.1.7

--- Template21/test.py
+++ Template21/test.py
@@ -2,10 +2,14 @@ import groupCounter
 
 counterA = groupCounter.groupCounter()
 
-counterA.readGjfFile(fileName='C9H18_100_r015.gjf', directory='Gjfs', moleculeLabel='test1')
+counterA.readGjfFile(fileName='C5H10_5.gjf', directory='Gjfs', moleculeLabel='test1')
+counterA.readGroupTemplate()
+counterA.writeDBGCVector(overwrite=True)
+
+counterA.readGjfFile(fileName='C9H18_100_r015.gjf', directory='Gjfs', moleculeLabel='test2')
 counterA.readGroupTemplate()
 counterA.writeDBGCVector(overwrite=False)
 
-counterA.readGjfFile(fileName='C10H22_57_r019.gjf', directory='Gjfs', moleculeLabel='test1')
+counterA.readGjfFile(fileName='C10H22_57_r019.gjf', directory='Gjfs', moleculeLabel='test3')
 counterA.readGroupTemplate()
 counterA.writeDBGCVector(overwrite=False)
