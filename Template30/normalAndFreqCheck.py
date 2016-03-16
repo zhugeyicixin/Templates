3c67c8347484ab322cd99c46d8c77bbae6b0498c
zhugeyicixin <hetanjin@163.com>
2016-03-16 14:16:12 +0800

    Tem.30.ver.2.0.1
    add the function to check imaginary frequencies

---
+++ Template30/normalAndFreqCheck.py
@@ -0,0 +1,61 @@
+# READING--------------------------------------------------------------------------------------
+# Read from specifically formatted excel sheet and store them as data arrays
+from numpy import *
+from xlrd import *
+from xlwt import *
+from re import *
+import re
+import os
+import shutil
+
+
+pattern_end = re.compile('^.*Normal termination of Gaussian 09.*$')
+pattern_freq = re.compile('^.*Frequencies -- *(-?[0-9]+\.[0-9]+)? *(-?[0-9]+\.[0-9]+)? *(-?[0-9]+\.[0-9]+)?$')
+
+error_file_num = 0
+pwd = os.getcwd()
+tmp_fileLists = os.listdir(pwd)
+for tmp_file in tmp_fileLists:
+	if os.path.isfile(tmp_file):
+		continue
+
+	print tmp_file
+	if os.path.exists(os.path.join(tmp_file, tmp_file+'.log')):
+		fr = file(os.path.join(tmp_file, tmp_file+'.log'), 'r')
+		tmp_lines = fr.readlines()
+		tmp_m = pattern_end.match(tmp_lines[-1])
+		if tmp_m:
+			freq = []
+			freq_done = -1
+			for tmp_line in tmp_lines:
+				if freq_done != 1:
+					tmp2_m = pattern_freq.match(tmp_line)
+					if tmp2_m:
+						freq.extend(map(float,tmp2_m.groups()))
+						freq_done = 0
+					if freq_done == 0:
+						if re.search('Thermochemistry', tmp_line):
+							while None in freq:
+								freq.remove(None)
+							freq_done = 1
+			for tmp_num in freq:
+				if tmp_num<=0:
+					print 'Error! An imaginary frequency!'
+					error_file_num += 1
+					print freq
+					break
+
+		else:
+			print 'Error! ' + tmp_file + ' did not end normally!'
+			error_file_num += 1				
+	else:
+		print 'Error! Log file not found!', tmp_file
+		error_file_num += 1		
+
+print 'error_file_num: ', error_file_num
+
+# THE END
+
+
+
+
